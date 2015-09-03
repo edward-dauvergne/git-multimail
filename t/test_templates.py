098b962f8bee16951227a74c477f7429635cfa62
Edward d'Auvergne <edward@nmr-relax.com>
2015-09-03 17:01:42 +0200

    Update of all system tests for the changed text.
    Signed-off-by: Edward d'Auvergne <edward@nmr-relax.com>

--- t/test_templates.py
+++ t/test_templates.py
@@ -12,8 +12,6 @@ git_multimail.Config.add_config_parameters('multimailhook.reponame=name<with>spe
 
 # A template with HTML in it:
 git_multimail.REVISION_INTRO_TEMPLATE = """\
-<span style="color:#808080">This is an automated email from the git hooks/post-receive script.</span><br /><br />
-
 <strong>%(pusher)s</strong> pushed a commit to %(refname_type)s %(short_refname)s
 in repository %(repo_shortname)s.<br />
 
@@ -24,11 +22,6 @@ in repository %(repo_shortname)s.<br />
 git_multimail.COMBINED_INTRO_TEMPLATE = git_multimail.REVISION_INTRO_TEMPLATE
 
 git_multimail.FOOTER_TEMPLATE = """\
-<br />
-<span style="color:#808080">-- <br />
-To stop receiving notification emails like this one, please contact
-%(administrator)s or <a href="http://example.com">click here</a>.
-</span>
 """
 
 git_multimail.REVISION_FOOTER_TEMPLATE = git_multimail.FOOTER_TEMPLATE
